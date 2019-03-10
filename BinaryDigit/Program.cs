5661d7a661ffcdb86763b404b200feec93be212d
Lev# <mr.lmartirosyan888@gmail.com>
2019-03-10 17:51:24 +0400

    relise one

---
+++ BinaryDigit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryDigith
+{
+    class Program
+    {
+        static void Converte(int n)
+        {
+            int temp = n % 2;
+
+            if (n >= 2)
+            {
+                Converte(n / 2);
+
+            }
+            Console.Write(temp);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Write a digit : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Converte(n);
+            Console.ReadKey();
+        }
+    }
+}
